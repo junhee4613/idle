fc0f0ab7fd424a675a76ed955c72134e9b296358
junhee4613 <junhee000123@naver.com>
2024-05-21 22:16:37 +0900

    중간 푸시

--- DragAndDrop/Assets/02.Scripts/Barrage/Projectile_spawner.cs
+++ DragAndDrop/Assets/02.Scripts/Barrage/Projectile_spawner.cs
@@ -8,14 +8,19 @@ public class Projectile_spawner : MonoBehaviour
     int repeat_num = 0;
     float projectile_speed = 0;
     Color projectile_color = Color.red;
-    Projectile_shape shape = Projectile_shape.NON;
+    GameObject projectile_obj;
     Spawner_mode spawner_mode = Spawner_mode.NON;
     Projectile_moving_mode moving_mode = Projectile_moving_mode.NON;
 
-    public void Init(int repeat_num, float projectile_speed, Color projectile_color, 
-        Projectile_shape shape, Spawner_mode spawner_mode, Projectile_moving_mode moving_mode)
+    public Projectile_spawner(int repeat_num, float projectile_speed, Color projectile_color, 
+        GameObject obj, Spawner_mode spawner_mode, Projectile_moving_mode moving_mode)
     {
-
+        this.repeat_num = repeat_num;
+        this.projectile_speed = projectile_speed;
+        this.projectile_color = projectile_color;
+        this.projectile_obj = obj;
+        this.spawner_mode = spawner_mode;
+        this.moving_mode = moving_mode;
     }
     // Start is called before the first frame update
     private void Awake()
@@ -38,50 +43,20 @@ public class Projectile_spawner : MonoBehaviour
     }
     void Projectile_spawn()
     {
+
         for (int i = 0; i < repeat_num; i++)
         {
-            switch (shape)
-            {
-                case Projectile_shape.CIRCLE:
-                    GameObject circel = Managers.Resource.Load<GameObject>("원");
-                    circel.transform.position = transform.position;
-                    circel.transform.rotation = Quaternion.Euler(new Vector3(0, 0 ,(360 / repeat_num) * i));
-                    circel.transform.localScale = Vector3.one;
-                    projectiles.Add(circel);
-                    Managers.Pool.Pop(circel);
-                    break;
-                case Projectile_shape.BOX:
-                    GameObject box = Managers.Resource.Load<GameObject>("박스");
-                    box.transform.position = transform.position;
-                    box.transform.rotation = Quaternion.Euler(new Vector3(0, 0, (360 / repeat_num) * i));
-                    box.transform.localScale = Vector3.one;
-                    projectiles.Add(box);
-                    Managers.Pool.Pop(box);
-                    break;
-                case Projectile_shape.TRIANGLE:
-                    GameObject triangle = Managers.Resource.Load<GameObject>("삼각형");
-                    triangle.transform.position = transform.position;
-                    triangle.transform.rotation = Quaternion.Euler(new Vector3(0, 0, (360 / repeat_num) * i));
-                    triangle.transform.localScale = Vector3.one;
-                    projectiles.Add(triangle);
-                    Managers.Pool.Pop(triangle);
-                    break;
-                case Projectile_shape.CAPSULE:
-                    GameObject capsule = Managers.Resource.Load<GameObject>("캡슐");
-                    capsule.transform.position = transform.position;
-                    capsule.transform.rotation = Quaternion.Euler(new Vector3(0, 0, (360 / repeat_num) * i));
-                    capsule.transform.localScale = Vector3.one;
-                    projectiles.Add(capsule);
-                    Managers.Pool.Pop(capsule);
-                    break;
-                case Projectile_shape.NON:
-                    Debug.Log("설정 안함");
-                    break;
-                default:
-                    Debug.Log("제공하지 않는 모양");
-                    break;
-            }
+            GameObject projectile = this.projectile_obj;
+            projectile.transform.position = transform.position;
+            projectile.transform.rotation = Quaternion.Euler(new Vector3(0, 0, (360 / repeat_num) * i));
+            projectile.transform.localScale = Vector3.one;
+            projectiles.Add(projectile);
+            Managers.Pool.Pop(projectile);
         }
+        Barrage_end();
+    }
+    void Barrage_end()
+    {
         switch (spawner_mode)
         {
             case Spawner_mode.REPEAT_END:
@@ -93,8 +68,6 @@ public class Projectile_spawner : MonoBehaviour
             default:
                 break;
         }
-
-
     }
     void Projectile_move()
     {
--- DragAndDrop/Assets/02.Scripts/Chapters/2/Chapter2_general_stage1.cs
+++ DragAndDrop/Assets/02.Scripts/Chapters/2/Chapter2_general_stage1.cs
@@ -10,13 +10,22 @@ public class Chapter2_general_stage1 : BossController
     public Cactus_climb_up cactus_climb_up;
     List<GameObject> general_warning_obj = new List<GameObject>();
     public Color warning_color;
-    public Cactus_Barrage cactus_barrage;
+    public Fruit_Barrage fruit_barrage;
     public Cactus_thorn cactus_thorn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        fruit_barrage.cactus = Managers.Resource.Load<GameObject>("Fruit_cactus");
+        Debug.Log(fruit_barrage.cactus.name);
+        for (int i = 0; i < fruit_barrage.cactus.transform.childCount; i++)
+        {
+            for (int j = 0; j < fruit_barrage.cactus.transform.GetChild(i).childCount; j++)
+            {
+                fruit_barrage.fruit_spawner_pos[] = fruit_barrage.cactus.transform.GetChild(i).GetChild(j).gameObject;
+                Debug.Log(fruit_barrage.cactus.transform.GetChild(i).GetChild(j).gameObject.name);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -24,24 +33,27 @@ public class Chapter2_general_stage1 : BossController
     public override void Pattern_processing()
     {
         Pattern_function(ref cactus_climb_up.pattern_data, ref cactus_climb_up.pattern_ending, ref cactus_climb_up.duration,ref cactus_climb_up.pattern_count, Cactus_climb_up_pattern);
-        Pattern_function(ref cactus_barrage.pattern_data, ref cactus_barrage.pattern_ending, ref cactus_barrage.duration,ref cactus_barrage.pattern_count, Cactus_barrage_pattern);
-        GameObject fruit = Managers.Resource.Load<GameObject>("선인장 열매");
-        fruit.transform.position =
-            cactus_barrage.fruit_spawner_pos[Random.Range(0, cactus_barrage.fruit_spawner_pos.Length)].transform.position;
+        Pattern_function(ref fruit_barrage.pattern_data, ref fruit_barrage.pattern_ending, ref fruit_barrage.duration,ref fruit_barrage.pattern_count, Fruit_barrage_pattern);
+        if(fruit_barrage.fruit == null/*여기에 열매가 날 자리들 중 빈자리가 있는지 확인*/)
+        {
+            fruit_barrage.fruit = Managers.Resource.Load<GameObject>("선인장 열매");
+        }
+        fruit_barrage.fruit.transform.position = fruit_barrage.fruit_spawner_pos[Random.Range(0, fruit_barrage.fruit_spawner_pos.Length)].transform.position;
+        Managers.Pool.Pop(fruit_barrage.fruit);
     }
     void Cactus_climb_up_pattern()
     {
         switch (cactus_climb_up.pattern_data[cactus_climb_up.pattern_count].action_num)
         {
             case 0:             //선인장 하나 생성 둥 때 나옴
-                //먼저 경고 장판 생성한뒤
                 cactus_climb_up.index_num = (sbyte)Random.Range(1, 4);
                 general_warning_obj[general_warning_obj.Count] = General_warning_box(new Vector3(cactus_climb_up.cactus_size[cactus_climb_up.index_num], 0.1f, 0), new Vector3(9 * cactus_climb_up.appearance_dir, cactus_climb_up.appearance_height, 0), warning_color);
                 break;
             case 1:
+                //랜덤한 선인장 하나 소환해서 위로 이동시키기
                 general_warning_obj[general_warning_obj.Count].SetActive(false);
                 general_warning_obj.RemoveAt(0);
-                GameObject cactus_obj = Managers.Pool.Pop(Managers.Resource.Load<GameObject>("여기에 나중에 선인장"));
+                GameObject cactus_obj = Managers.Resource.Load<GameObject>("여기에 나중에 선인장");
                 cactus_obj.transform.position = new Vector3(9 * cactus_climb_up.appearance_dir, cactus_climb_up.appearance_height, 0);
                 cactus_obj.transform.DOScale(Vector3.one * 1.5f, 0.2f).OnComplete(() =>
                 {
@@ -59,38 +71,40 @@ public class Chapter2_general_stage1 : BossController
                 break;
         }
     }
-    void Cactus_barrage_pattern()
+    void Fruit_barrage_pattern()
     {
-        switch (cactus_barrage.pattern_data[cactus_barrage.pattern_count].action_num)
+        switch (fruit_barrage.pattern_data[fruit_barrage.pattern_count].action_num)
         {
             case 0:             //선인장 등장
-                cactus_barrage.cactus = Managers.Pool.Pop(Managers.Resource.Load<GameObject>("탄막 선인장"));
-                cactus_barrage.cactus.transform.position = new Vector3(0, 0, 0);
-                cactus_barrage.cactus.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
-                cactus_barrage.cactus.transform.DOScaleY(10, 0).OnComplete(() =>
+
+                fruit_barrage.cactus = Managers.Resource.Load<GameObject>("탄막 선인장");
+                fruit_barrage.cactus.transform.position = new Vector3(0, 0, 0);
+                fruit_barrage.cactus.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+                Managers.Pool.Pop(fruit_barrage.cactus);
+                fruit_barrage.cactus.transform.DOScaleY(10, 0).OnComplete(() =>
                 {
-                    cactus_barrage.cactus.transform.DOScaleY(10, 0);
+                    fruit_barrage.cactus.transform.DOScaleY(10, 0);
                 });
                 break;
             case 1:     //열매 떨어트림
-                cactus_barrage.random_num = Random.Range(0, cactus_barrage.fully_grown.Count);
+                fruit_barrage.random_num = Random.Range(0, fruit_barrage.fully_grown.Count);
 
-                if (cactus_barrage.fully_grown[cactus_barrage.random_num].TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+                if (fruit_barrage.fully_grown[fruit_barrage.random_num].TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
                 {
                     rb.gravityScale = 1;
-                    cactus_barrage.fully_grown.Remove(rb.gameObject);
+                    fruit_barrage.fully_grown.Remove(rb.gameObject);
                 }
                 break;
             case 2:
                 // 열매 터짐
-                Managers.Pool.Push(cactus_barrage.fully_grown[cactus_barrage.random_num]);
-                cactus_barrage.fully_grown.RemoveAt(cactus_barrage.random_num);
+                Managers.Pool.Push(fruit_barrage.fully_grown[fruit_barrage.random_num]);
+                fruit_barrage.fully_grown.RemoveAt(fruit_barrage.random_num);
                 //여기에 탄막 생성하는 로직 추가
                 break;
             case 3:     //선인장 사라짐
-                cactus_barrage.cactus.transform.DOScaleY(10, 0).OnComplete(() =>
+                fruit_barrage.cactus.transform.DOScaleY(10, 0).OnComplete(() =>
                 {
-                    cactus_barrage.cactus.transform.DOScaleY(10, 0);
+                    fruit_barrage.cactus.transform.DOScaleY(10, 0);
                 });
                 break;
         }
@@ -109,13 +123,14 @@ public class Chapter2_general_stage1 : BossController
         public sbyte index_num;             //몇번째 선인장을 가져올건지 고를 변수
     }
     [Serializable]
-    public class Cactus_Barrage : Pattern_base_data 
+    public class Fruit_Barrage : Pattern_base_data 
     {
-        public GameObject[] fruit_spawner_pos;
+        public List<GameObject> fruit_spawner_pos = new List<GameObject>();
         public float barrage_bullet_num;
         public GameObject cactus;
         public List<GameObject> fully_grown = new List<GameObject>();
         public int random_num;
+        public GameObject fruit;
     }
     [Serializable]
     public class Cactus_thorn : Pattern_base_data
